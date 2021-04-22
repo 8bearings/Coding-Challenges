8eda81de3330f3c680a5b91a94645936be45604b
Elliott Watson <elliottwatson0@gmail.com>
2021-04-22 12:45:12 -0400

    4/22/21

---
+++ 20.js
@@ -0,0 +1,8 @@
+// You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+
+function check(a,x){
+    if(a.includes(x) === true ){
+      return true
+    }else
+      return false
+  };

1b18f214d1854b15f8d6fd69566f692721fd6282
Elliott Watson <elliottwatson0@gmail.com>
2021-05-09 00:56:53 -0400

    5/9/21

--- 2.js
+++ 2.js
@@ -1,6 +1,5 @@
-// Please create a function that takes in a number and a word. 
-// If the length of the word multiplied by the number passed into the function is greater than 100, alert "WINNER"! 
-
+/* Please create a function that takes in a number and a word. 
+If the length of the word multiplied by the number passed into the function is greater than 100, alert "WINNER"! */
 
 function numWord(n1,w1){
     if (w1.length * n1 > 100){

2fede08d0954871873b410a4228ff00dfbd8f690
Elliott Watson <elliottwatson0@gmail.com>
2021-05-10 00:58:06 -0400

    5/10/21

--- 7.js
+++ 7.js
@@ -1,5 +1,5 @@
-// Grade book
-// Complete the function so that it finds the mean of the three scores passed to it and returns the letter value associated with that grade.
+/* Grade book
+Complete the function so that it finds the mean of the three scores passed to it and returns the letter value associated with that grade. */
 
 function getGrade (s1, s2, s3) {
     if(((s1 + s2 + s3)/3) >= 90 ){

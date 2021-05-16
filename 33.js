711eb6759c7a5a97fa504a4035ab787b018ec0f9
Elliott Watson <elliottwatson0@gmail.com>
2021-05-16 03:18:52 -0400

    5/16/21

--- 33.js
+++ 33.js
@@ -2,3 +2,10 @@
 // You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed 'age' of 0. Return a new array with [youngest age, oldest age, difference between the youngest and oldest age].
 
 
+function orderAges(array) {
+
+          }
+          console.log(orderAges([23, 3, 10, 54]))
+
+
+          

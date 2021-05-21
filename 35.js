e058085093eaebbbd851afcaaa1c2b955c97a703
Elliott Watson <elliottwatson0@gmail.com>
2021-05-21 02:07:32 -0400

    5/21/21

--- 35.js
+++ 35.js
@@ -1,11 +1,9 @@
+// Count the number of vowels
+
 function vowelsCounter(text) {
-   
     // Search text with Regex and store all matching instances 
     let matchingInstances = text.match(/[aeiou]/gi);
-
-    // Check if matching instances exist then calculate length
-    if (matchingInstances) {    
-        // Return number of vowels
+        if (matchingInstances) {    
         return matchingInstances.length
     } else {
         return 0

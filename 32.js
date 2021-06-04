3ef8f96a1d87263a71980817753392d00c7c3a99
Elliott Watson <elliottwatson0@gmail.com>
2021-06-04 04:15:39 -0400

    6/4/21

--- 32.js
+++ 32.js
@@ -2,14 +2,6 @@
 // Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 // [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 
-// function multiplyOrder(array) {
-//     let newArr = array.forEach(element => {
-//         element * element 
-//     });
-//     console.log(newArr)
-//           }
-          
-
           function multiplyOrder(array) {
             let newArr = array.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
             return newArr

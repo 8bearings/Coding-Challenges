1375d1ecdc5e3e22c2deb4cb0f6dfc292fb12fe9
Elliott Watson <elliottwatson0@gmail.com>
2021-06-23 00:12:22 -0400

    6/23/21

--- 37.js
+++ 37.js
@@ -9,7 +9,7 @@ So friendship is twice stronger than love :-)
 The input will always be in lowercase and never be empty. */
 
 function wordsToMarks(string){
-  var sum = 0;
+  let sum = 0;
   for (let i = 0; i < string.length; i++)
     sum += str.charCodeAt(i) - 96;
   return sum;

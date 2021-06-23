dcf8ae9e7985e303f969c51205e9a2be2a96f7d2
Elliott Watson <elliottwatson0@gmail.com>
2021-06-22 23:51:41 -0400

    6/22/21

--- 37.js
+++ 37.js
@@ -7,3 +7,7 @@ and f + r + i + e + n + d + s + h + i + p = 108
 So friendship is twice stronger than love :-)
 
 The input will always be in lowercase and never be empty. */
+
+function wordsToMarks(string){
+    [𝕒𝕓𝕔𝕕𝕖𝕗𝕘𝕙𝕚𝕛𝕜𝕝𝕞𝕟𝕠𝕡𝕢𝕣𝕤𝕥𝕦𝕧𝕨𝕩𝕪𝕫𝔸𝔹]
+  }

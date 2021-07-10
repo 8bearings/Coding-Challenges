ad4456bb5002da9c3ccb4f7b283424ddec7903b2
Elliott Watson <elliottwatson0@gmail.com>
2021-07-10 14:42:54 -0400

    7/10/21

--- 40.js
+++ 40.js
@@ -9,3 +9,11 @@ Test.assertEquals(solve([[1,2],[4,4],[5,6,6]]),4);
 Test.assertEquals(solve([[1,2],[3,4],[5,6]]),8);
 Test.assertEquals(solve([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]]),72); */
 
+function solve(arr) {
+    return arr.map(a => {
+      let s = new Set();
+      a.forEach(el => s.add(el));
+      return s.size;
+    })
+      .reduce((out, n) => out * n, 1);
+  }

0d4a90639440b6b79e6c6badc8270815c1e3ba78
Elliott Watson <elliottwatson0@gmail.com>
2021-07-14 18:41:41 -0400

    7/14/21

--- 42.js
+++ 42.js
@@ -1,3 +1,6 @@
-function domainName(url){
+//Write a function that when given a URL as a string, parses out just the domain name and returns it as a string.
 
-}
+function domainName(url){
+   let splitStr = url.replace("http://","").replace("https://","").replace("www.","").split('.').shift()
+   return splitStr
+  }

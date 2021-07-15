8f4be2db26e9e1a335533b5320578320476390ac
Elliott Watson <elliottwatson0@gmail.com>
2021-07-15 18:15:29 -0400

    7/15/21

---
+++ 43.js
@@ -0,0 +1,15 @@
+/* Given a string, return a new string that has transformed based on the input:
+
+Change case of every character, ie. lower case to upper case, upper case to lower case.
+Reverse the order of words from the input. */
+
+function stringTransformer(str) {
+    let reverseStr = str
+    .split(" ")
+    .reverse()
+    .join(' ')
+    .split('')
+    .map(l => l === l.toUpperCase() ? l.toLowerCase() : l.toUpperCase())
+    .join('')
+    return reverseStr
+    }

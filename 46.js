47dc478f17ae8fc9175c2230cbcc13b86727a0b1
Elliott Watson <elliottwatson0@gmail.com>
2021-07-27 18:29:54 -0400

    7/27/21

--- 46.js
+++ 46.js
@@ -1,2 +1,7 @@
 // You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
 
+function sortArray(array) {
+    let oddNums = array.filter( x => x % 2).sort((a,b) => a-b)
+    return array.map(n => n % 2 ? oddNums.shift() : n )
+  }
+  

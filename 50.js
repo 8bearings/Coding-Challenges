307cb749c7461041ec7f26eeb41e2fa0225f4411
Elliott Watson <elliottwatson0@gmail.com>
2021-08-04 18:11:48 -0400

    8/4/21

---
+++ 50.js
@@ -0,0 +1,19 @@
+/* Your job is to figure out the index of which vowel is missing from a given string:
+
+A has an index of 0,
+E has an index of 1,
+I has an index of 2,
+O has an index of 3,
+U has an index of 4.
+Notes: There is no need for string validation and every sentence given will contain all vowles but one. Also, you won't need to worry about capitals. */
+
+
+
+function absentVowel(x){
+    const v = 'aeiou'
+    for(let i in v){
+      if(x.toLowerCase().indexOf(v[i]) === -1){
+        return parseInt(i)
+      }
+    }
+  }

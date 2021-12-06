4930c8b2b420426242d21b56c6f429439f666f77
Elliott Watson <elliottwatson0@gmail.com>
2021-12-06 01:07:38 -0500

    12/6/21

--- 101.js
+++ 101.js
@@ -1,3 +1,10 @@
 /* you are asked to square every digit of a number and concatenate them.
 
 For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. */
+
+function squareDigits(num){
+    let str = num.toString()
+    let split = str.split('')
+    let newStr = split.map(num=> num**2)
+    return Number(newStr.join(''))
+  }

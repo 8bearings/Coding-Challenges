46235ab4065e61e2f1df8b78a625e3e3db7362f9
Elliott Watson <elliottwatson0@gmail.com>
2021-12-24 00:56:07 -0500

    12/24/21

--- 106.js
+++ 106.js
@@ -3,6 +3,5 @@
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
 function squareSum(numbers){
-    return numbers.reduce((a,b)=> (a + (b**2)),0)
-            
+    return numbers.reduce((a,b)=> (a + (b**2)),0)         
  }

3fb65e4b9b166a978221a8c4b7de176b007527c2
Elliott Watson <elliottwatson0@gmail.com>
2022-01-05 22:52:32 -0500

    1/5/22

---
+++ 107.js
@@ -0,0 +1,7 @@
+// Given a sequence of numbers, find the largest pair sum in the sequence.
+
+function largestPairSum (numbers) {
+    let sorted = numbers.sort((a,b)=>(b-a))
+    return sorted[0]+sorted[1]
+  }
+  

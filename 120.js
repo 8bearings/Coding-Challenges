142a5c56eccbe1d6222f3973d373a23e3298d9d9
Elliott Watson <elliottwatson0@gmail.com>
2022-02-06 01:08:24 -0500

    2/6/22

---
+++ 120.js
@@ -0,0 +1,5 @@
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+function sumStr(a,b) {
+    let math= Number(1 * a + b * 1 )
+    return math.toString()
+    }

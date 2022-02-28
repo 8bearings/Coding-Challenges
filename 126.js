b76dc5f5ea04b75941215b3520374c44935a55fe
Elliott Watson <elliottwatson0@gmail.com>
2022-02-28 00:28:55 -0500

    2/28/22

---
+++ 126.js
@@ -0,0 +1,11 @@
+// Give you a function animal, accept 1 parameter:obj like this:
+
+function animal(obj){
+    let name = obj.name 
+    let leg = obj.legs
+    let color = obj.color
+    
+ return  `This ${color} ${name} has ${leg} legs.`
+ }
+ 
+ 
